93efe436575de9841f0875712f6595ace0ed807d
Kailash Manandhar <manandharkailash1@gmail.com>
2026-07-17 11:00:58 +0545

    added projects page

---
+++ script.js
@@ -0,0 +1,23 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+sections.forEach(section => {
+
+    section.classList.add("fade");
+    observer.observe(section);
+
+});
